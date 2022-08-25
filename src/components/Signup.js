22cb8c190bbed4cfcf18758ff6820f6c65f97cff
jonz0 <ijonluu@gmail.com>
2022-08-25 00:10:23 -0700

    fix: failed load fixed. logins working, TODO: error messages

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,6 @@
 import userEvent from "@testing-library/user-event";
 import React, { useRef, useState } from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import { Form, Button, Card, Alert } from "react-bootstrap";
 import "./Signup.css";
 import { useAuth } from "../contexts/AuthContext";
 
@@ -10,6 +10,7 @@ export default function Signup() {
   const passwordConfirmRef = useRef();
   const { signup } = useAuth();
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -20,10 +21,12 @@ export default function Signup() {
 
     try {
       setError("");
+      setLoading(true);
       await signup(emailRef.current.value, passwordRef.current.value);
     } catch (error) {
       setError("Failed to create an account");
     }
+    setLoading(false);
   }
 
   return (
@@ -31,13 +34,14 @@ export default function Signup() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign up</h2>
-          <Form>
-            <Form.Group class="input-container" id="email">
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required></Form.Control>
             </Form.Group>
 
-            <Form.Group class="input-container" id="password">
+            <Form.Group id="password">
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type="password"
@@ -46,7 +50,7 @@ export default function Signup() {
               ></Form.Control>
             </Form.Group>
 
-            <Form.Group class="input-container" id="password-confirm">
+            <Form.Group id="password-confirm">
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control
                 type="password"
@@ -54,7 +58,7 @@ export default function Signup() {
                 required
               ></Form.Control>
             </Form.Group>
-            <Button className="w-100" type="submit">
+            <Button disabled={loading} className="w-100" type="submit">
               Sign up
             </Button>
           </Form>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -4,11 +4,11 @@ import { auth } from "../firebase";
 const AuthContext = React.createContext();
 
 export function useAuth() {
-  return useContext;
+  return useContext(AuthContext);
 }
 
 export default function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState;
+  const [currentUser, setCurrentUser] = useState();
 
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
