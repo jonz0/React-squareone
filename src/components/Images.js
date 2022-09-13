532eef672f0a5708cca03983e00a95ba78a47fa7
jonz0 <ijonluu@gmail.com>
2022-09-13 11:56:34 -0700

    feat: user json linked to firestore

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { storage, db, auth } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useAuth } from "../contexts/AuthContext";
 import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
@@ -9,35 +9,37 @@ import "../css/Images.css";
 export default function Images() {
   const [imageUpload, setImageUpload] = useState(null);
   const [imageList, setImageList] = useState([]);
-  const imageListRef = ref(storage, "images/");
   const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
   const [age, setAge] = useState();
   const { currentUser, logout } = useAuth();
+  const currentUserId = currentUser.uid + "/";
+  const docRef = doc(db, "users", currentUser.uid);
 
-  function uploadImage() {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + uuidv4()}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageList((prev) => [...prev, url]);
+  async function displayUserImages() {
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const imageListRef = docSnap.data().userStorageRef;
+
+      // Displays images stored in uid/images/
+      listAll(ref(storage, imageListRef)).then((response) => {
+        response.items.forEach((item) => {
+          getDownloadURL(item).then((url) => {
+            setImageList((prev) => [...prev, url]);
+          });
+        });
       });
-    });
+    } else {
+      console.log("Error: please contact the big boss");
+    }
   }
 
   useEffect(() => {
     handleDataDisplay();
-    listAll(imageListRef).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageList((prev) => [...prev, url]);
-        });
-      });
-    });
+    displayUserImages();
   }, []);
 
   async function handleDataDisplay() {
-    const docRef = doc(db, "users", currentUser.uid);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       setFirstName(docSnap.data().firstName);
@@ -48,14 +50,28 @@ export default function Images() {
     }
   }
 
-  // const userRef = doc(db, "users", currentUser.uid);
-  // setDoc(
-  //   userRef,
-  //   {
-  //     singleImageUrl:
-  //   },
-  //   { merge: true }
-  // );
+  const userRef = doc(db, "users", currentUser.uid);
+  function uploadImage() {
+    if (imageUpload == null) return;
+    const imageName = `${currentUserId}images/${imageUpload.name + uuidv4()}`;
+    const imageRef = ref(storage, imageName);
+
+    // Uploads image to firestore
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, url]);
+      });
+    });
+
+    // Adds image url to firebase user doc
+    setDoc(
+      userRef,
+      {
+        userStorageRef: currentUserId + "images/",
+      },
+      { merge: true }
+    );
+  }
 
   return (
     <div>
